5bc3e5751e48ad89204e8d8a8aa175db871c28a7
angelscutipa <angel.cutipa@ucsp.edu.pe>
2020-04-11 00:49:00 -0500

    Add files via upload

---
+++ ejercicio2/CMakeFiles/ejercicio2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio2.dir/ejercicio2.cpp.o"
+  "ejercicio2.pdb"
+  "ejercicio2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
